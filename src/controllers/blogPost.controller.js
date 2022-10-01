cb48300b00c95184ae259a00f06d33a10933bb1f
GusCabraal <gustavocabral97@hotmail.com>
2022-10-01 15:15:27 -0300

    refactor: organização do blogpost controller

--- src/controllers/blogPost.controller.js
+++ src/controllers/blogPost.controller.js
@@ -1,105 +1,93 @@
 const { blogPostService, categoryService } = require('../services');
+const { isPostExists, canUserEditAPost } = require('../utils');
 
 const published = new Date();
 const updated = new Date();
 
-const ERROR_MESSAGE = {
-  message: 'Ocorreu um erro',
-};
-
-const isPostExists = async (id) => {
-  const blogPost = await blogPostService.getById(id);
-  console.log(blogPost);
-  if (blogPost) return blogPost;
-  return false;
-};
-
-const cannotUserEditAPost = async (userId, postId) => {
-  const { dataValues: { user: { id } } } = await blogPostService.getById(postId);
-
-    if (id !== userId) return true;
-    return false;
-};
-
-const createPost = async (req, res) => {
+const createPost = async (req, res, next) => {
   try {
     const { id: userId } = req.user;
 
-    const result = await categoryService.allCategoryExists(req.body.categoryIds);
+    const result = await categoryService.allCategoryExists(
+      req.body.categoryIds,
+    );
     if (result) {
       return res.status(400).json({ message: '"categoryIds" not found' });
     }
-    
+
     const newPost = { ...req.body, userId, published, updated };
     const newBlogPost = await blogPostService.insert(newPost);
     return res.status(201).json(newBlogPost);
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
-const updatedPost = async (req, res) => {
+const updatedPost = async (req, res, next) => {
   try {
     const { id } = req.params;
     const { id: userId } = req.user;
 
-    const result = await cannotUserEditAPost(userId, id);
-    if (result) return res.status(401).json({ message: 'Unauthorized user' });
-   
+    await canUserEditAPost(userId, id);
+
     const updatePostContent = { ...req.body, userId, updated };
+    
     await blogPostService.updatePost(id, updatePostContent);
+    
     const updatedBlogPost = await blogPostService.getById(id);
     updatedBlogPost.userId = userId;
+    
     return res.status(200).json(updatedBlogPost);
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
-const getAll = async (_req, res) => {
+const getAll = async (_req, res, next) => {
   try {
     const blogPosts = await blogPostService.getAll();
     return res.status(200).json(blogPosts);
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
-const getById = async (req, res) => {
+const getById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await isPostExists(id);
-    if (!result) return res.status(404).json({ message: 'Post does not exist' });
-    return res.status(200).json(result);
+    
+    await isPostExists(id);
+    
+    const blogPost = await blogPostService.getById(id);
+    return res.status(200).json(blogPost);
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
-const getByText = async (req, res) => {
+const getByText = async (req, res, next) => {
   try {
     const { q } = req.query;
     const blogPost = await blogPostService.getByText(q);
     return res.status(200).json(blogPost);
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
-const removeById = async (req, res) => {
+const removeById = async (req, res, next) => {
   try {
-    const { id } = req.params;
+    const { id: postId } = req.params;
     const { id: userId } = req.user;
-    const result = await cannotUserEditAPost(userId, id);
-    if (result) return res.status(401).json({ message: 'Unauthorized user' });
 
-    const postExists = await isPostExists(id);
-    if (postExists) return res.status(404).json({ message: 'Post does not exist' });
-    
-    await blogPostService.deletePost(id);
+    await isPostExists(postId);
+
+    await canUserEditAPost(userId, postId);
+
+    await blogPostService.deletePost(postId);
     return res.status(204).end();
   } catch (e) {
-    res.status(500).json(ERROR_MESSAGE);
+    next(e);
   }
 };
 
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+const isPostExists = require('./isPostExists');
+const canUserEditAPost = require('./canUserEditAPost');
+
+module.exports = {
+    isPostExists,
+    canUserEditAPost,
+};
